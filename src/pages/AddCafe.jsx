34c18164764b52dc1457b4b3023c1e7f387b02e3
Sho sho <96018697+Showshow3w@users.noreply.github.com>
2026-08-01 03:10:37 -0400

    add routing, cafe list and cafe detail pages

---
+++ src/pages/AddCafe.jsx
@@ -0,0 +1,4 @@
+function AddCafe(){
+    return <h1>Adding Cafe Page</h1>
+}
+export default AddCafe
---
+++ src/pages/EditCafe.jsx
@@ -0,0 +1,5 @@
+function EditCafe() {
+  return <h1>Edit Cafe Page</h1>
+}
+
+export default EditCafe
